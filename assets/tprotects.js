0d1e53475e7929841458955e553e49aa22057b3f
繆迦徽棠 <miaojazephyris@gmail.com>
2025-07-03 21:34:46 +0800

    protects.js

--- assets/tprotects.js
+++ assets/tprotects.js
@@ -1,19 +1,14 @@
-// 禁止右鍵，但放行留言板
+// 禁止右鍵
 document.addEventListener("contextmenu", function (e) {
-  if (!e.target.closest('#cusdis_thread')) e.preventDefault();
 });
 
-// 禁止選字，但放行留言板
+// 禁止選字
 document.addEventListener("selectstart", function (e) {
-  if (!e.target.closest('#cusdis_thread')) e.preventDefault();
 });
 
-// 禁止複製，但放行留言板
+// 禁止複製
 document.addEventListener("copy", function (e) {
-  if (!e.target.closest('#cusdis_thread')) {
-    e.preventDefault();
     alert("僅供閱覽");
-  }
 });
 
 // 偵測爬蟲 UA（user-agent）
